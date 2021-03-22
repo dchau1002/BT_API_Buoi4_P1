8277081ca1783427f5517499cfecd5fe38c673f9
dchau1002 <71169357+dchau1002@users.noreply.github.com>
2021-03-22 23:00:59 +0700

    nopbai

---
+++ WebApp1/WebApp1/Models/db.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApp1.Models
+{
+    public class db
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=DemoCRUD;Integrated Security=True");
+
+        //select
+        public DataSet Empget(Employee emp, out string msg)
+        {
+            DataSet ds = new DataSet();
+            msg = "";
+            try
+            {
+                SqlCommand com = new SqlCommand("Sp_Employee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Sr_no", emp.Sr_no);
+                com.Parameters.AddWithValue("@Emp_name", emp.Emp_name);
+                com.Parameters.AddWithValue("@City", emp.City);
+                com.Parameters.AddWithValue("@SATE", emp.State);
+                com.Parameters.AddWithValue("@Country", emp.Country);
+                com.Parameters.AddWithValue("@Department", emp.Department);
+                com.Parameters.AddWithValue("@flag", emp.flag);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                da.Fill(ds);
+                msg = "OK";
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+                return ds;
+            }
+        }
+
+        //insert va cap nhat
+        public string Empdml(Employee emp, out string msg)
+        {
+            msg = "";
+            try
+            {
+                SqlCommand com = new SqlCommand("Sp_Employee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Sr_no", emp.Sr_no);
+                com.Parameters.AddWithValue("@Emp_name", emp.Emp_name);
+                com.Parameters.AddWithValue("@City", emp.City);
+                com.Parameters.AddWithValue("@SATE", emp.State);
+                com.Parameters.AddWithValue("@Country", emp.Country);
+                com.Parameters.AddWithValue("@Department", emp.Department);
+                com.Parameters.AddWithValue("@flag", emp.flag);
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                msg = "OK";
+                return msg;
+            }
+            catch (Exception ex)
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                msg = ex.Message;
+                return msg;
+            }
+        }
+    }
+}
